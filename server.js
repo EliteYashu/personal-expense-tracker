09d76d7b39066fc6fff272df7d7cc1c50e8838e3
Yashu Soni <soniyashu083@gmail.com>
2026-05-12 14:10:30 +0530

    Redirect root to expense tracker

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
+app.get('/', (req, res) => {
+  res.redirect('/HTML/personal_expense.html');
+});
+
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/personal_expense_tracker';
 
 mongoose.connect(MONGODB_URI)
